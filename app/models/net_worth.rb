9b4899311b3197c630335676892cf2a3a3e47775
Raghu Betina <raghu@firstdraft.com>
2016-11-16 22:55:18 +0000

    Direct association between liabilities and net_worths

--- app/models/net_worth.rb
+++ app/models/net_worth.rb
@@ -1,6 +1,9 @@
 class NetWorth < ApplicationRecord
   # Direct associations
 
+  belongs_to :liabilities_total,
+             :class_name => "Liability"
+
   belongs_to :asset_total,
              :class_name => "Asset"
 

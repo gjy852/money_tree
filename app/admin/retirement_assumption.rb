95a693a87e864b35afe063cea01cf2238afae918
Raghu Betina <raghu@firstdraft.com>
2016-11-16 22:55:49 +0000

    Whitelist columns in RetirementAssumption for ActiveAdmin

--- app/admin/retirement_assumption.rb
+++ app/admin/retirement_assumption.rb
@@ -1,5 +1,7 @@
 ActiveAdmin.register RetirementAssumption do
 
+ permit_params :age_id, :last_year_saving_id, :investment_return, :inflation, :increase_in_savings, :annual_savings, :initial_savings, :retirement_income, :retirement_age, :user_id
+
 # See permitted parameters documentation:
 # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
 #
